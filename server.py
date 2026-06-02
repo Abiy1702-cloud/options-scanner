7f9fb43df3cb31951730f150a654c16b9a2d2aff
Abiy1702-cloud <ab1702us@gmail.com>
2026-06-01 21:51:18 -0500

    initial 2

--- server.py
+++ server.py
@@ -15,7 +15,8 @@ except ImportError:
     print("\n  ERROR: pip install yfinance flask\n"); raise SystemExit(1)
 
 app = Flask(__name__, static_folder='.')
-PORT = 8765
+import os
+PORT = int(os.environ.get('PORT', 8765))
 
 VALID_PERIODS   = {'1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'}
 VALID_INTERVALS = {'1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'}
